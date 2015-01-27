646079baebe1c62e522fcb7ab87fb31f90b71695
HansS <hans.schenker@netxpert.ch>
2015-01-27 15:11:14 +0100

    cors added

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var cors = require('cors');
 var todos = require('./routes/todos');
 
 var db = require('./modules/db');
 var app = express();
 
 
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
